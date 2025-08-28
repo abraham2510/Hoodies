eb78c478f0d2fca66468ab31cbfc027bb58c21be
Abraham <abrahambillclinton@.com>
2025-08-28 06:56:36 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+  tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ["Inter", "sans-serif"],
+          roboto: ["Roboto", "sans-serif"],
+          bebas: ["Bebas Neue", "sans-serif"],
+        },
+      },
+    },
+  };
